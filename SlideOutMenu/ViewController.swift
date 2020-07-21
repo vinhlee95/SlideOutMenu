692b2f0fc8bbf9d3f30f5320c01f6f456c08903e
Vinh Le <vinh@vertics.co>
2020-07-21 06:56:45 +0300

    Initial Commit

---
+++ SlideOutMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideOutMenu
+//
+//  Created by Vinh Le on 7/21/20.
+//  Copyright © 2020 Vinh Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
