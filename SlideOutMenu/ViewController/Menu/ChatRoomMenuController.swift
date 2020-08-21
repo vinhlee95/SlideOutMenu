63258482294aab108e4d0ad46bcf5bffb4d88f5e
Vinh Le <vinh@vertics.co>
2020-08-21 05:44:05 +0300

    feat: add functionality of filtering groups by text

--- SlideOutMenu/ViewController/Menu/ChatRoomMenuController.swift
+++ SlideOutMenu/ViewController/Menu/ChatRoomMenuController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
-class ChatRoomMenuController: UITableViewController, UISearchBarDelegate {
+class ChatRoomMenuController: UITableViewController {
     private let cellId = "cellId"
     private let data = [
         ["generals", "introductions"],
         ["jobs"],
         ["Ronaldo", "Federer", "Nadal"]
     ]
+    private var filteredData = [[String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.filteredData = self.data
         tableView.separatorStyle = .none
         tableView.backgroundColor = #colorLiteral(red: 0.2980392157, green: 0.2078431373, blue: 0.2862745098, alpha: 1)
     }
@@ -41,16 +43,16 @@ class ChatRoomMenuController: UITableViewController, UISearchBarDelegate {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return data.count
+        return filteredData.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data[section].count
+        return filteredData[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = ChatroomMenuCell(style: .default, reuseIdentifier: cellId)
-        let text = data[indexPath.section][indexPath.row]
+        let text = filteredData[indexPath.section][indexPath.row]
         let firstText = AttributedText(text: "#  ", fontSize: 18, fontWeight: .regular, color: #colorLiteral(red: 0.4745098039, green: 0.4078431373, blue: 0.4666666667, alpha: 1))
         let secondText = AttributedText(text: text, fontSize: 18, fontWeight: .bold, color: .white)
         cell.textLabel?.appendAttributedText(firstText: firstText, secondText: secondText)
@@ -59,8 +61,20 @@ class ChatRoomMenuController: UITableViewController, UISearchBarDelegate {
     }
 }
 
-extension ChatRoomMenuController {
+//
+// Filtering logics
+//
+extension ChatRoomMenuController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+        if searchText.isEmpty {
+            filteredData = data
+        } else {
+            filteredData = data.map({ (group) -> [String] in
+                return group.filter
+                    {$0.lowercased().contains(searchText.lowercased())} // $0 is shorthand for first argument in this closure
+            })
+        }
+        
+        tableView.reloadData()
     }
 }
